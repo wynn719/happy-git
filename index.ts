d49ce8f685a3c70ab40b48c359e273f67b7f9435
wynn719 <762671261@qq.com>
2024-09-12 19:14:54 +0800

    feat: auto copy hotfix

--- index.ts
+++ index.ts
@@ -3,7 +3,22 @@ import prompts from "prompts";
 import Commander from "commander";
 import pc from "picocolors";
 import packageJson from "./package.json";
-import { awaitSleep, exec } from "./src/utils";
+import util from "util";
+import cp from "child_process";
+
+export function awaitSleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export async function exec(cmd: string) {
+  const res = await util.promisify(cp.exec)(cmd);
+
+  if (res.stderr) {
+    throw res.stderr;
+  }
+
+  return res.stdout?.trim();
+}
 
 const program = new Commander.Command(packageJson.name)
   .description(packageJson.description)
